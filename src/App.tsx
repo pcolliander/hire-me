08c244e8ae254990ed3ac7e6878e7ebbe4c3525e
pcolliander <pontus.colliander@gmail.com>
2020-09-15 13:47:08 +0200

    created a folder for the css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Avatar from './avatar';
-import './style.css';
+import './css/style.css';
 
 const accessToken = '234ffdb8-0889-4be3-b096-97ab1679752c';
 const groupId = '11fc220c-ebba-4e55-9346-cd1eed714620';
