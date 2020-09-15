b5b57c341da028ac0be2578bb3b12f2d994ee4d2
pcolliander <pontus.colliander@gmail.com>
2020-09-15 18:01:48 +0200

    checked-in icon to make it clearer

--- src/avatar.tsx
+++ src/avatar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import SignIn from './sign-in';
+import checkmark from './checkmark.svg';
 
 export default function Avatar(child: Child) {
   const [showSignIn, setShowSignIn] = useState(false);
@@ -7,8 +8,9 @@ export default function Avatar(child: Child) {
   return (
     <>
       <div onClick={() => setShowSignIn(true)} className={`child ${child.checkedIn ? "checked-in" : ""}`}>
-        <img src={child.image.small} alt='' />
+        <img className='avatar' src={child.image.small} alt='' />
         <p>{child.name.firstName}</p>
+        { child.checkedIn && <img className='checkmark' src={checkmark} /> }
       </div>
 
       { showSignIn &&
