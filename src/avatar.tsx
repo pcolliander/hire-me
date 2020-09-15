bcb55635ffe0582fb8c099c1e28ed16068ffa52a
pcolliander <pontus.colliander@gmail.com>
2020-09-15 16:04:15 +0200

    indicate a child is checked-in

--- src/avatar.tsx
+++ src/avatar.tsx
@@ -6,8 +6,8 @@ export default function Avatar(child: Child) {
 
   return (
     <>
-      <div onClick={() => setShowSignIn(true)} className='child'>
-        <img src={child.image.small} alt="" />
+      <div onClick={() => setShowSignIn(true)} className={`child ${child.checkedIn ? "checked-in" : ""}`}>
+        <img src={child.image.small} alt='' />
         <p>{child.name.firstName}</p>
       </div>
 
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,6 @@
 
 type Child = {
+  checkedIn: Boolean;
   childId: string;
   name: { firstName: string, fullName: string };
   image: { small: string };
