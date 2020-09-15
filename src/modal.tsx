f5d66fabcdf1b2d97aecd9ffaafb2b9a497b05a8
pcolliander <pontus.colliander@gmail.com>
2020-09-15 13:43:21 +0200

    do not scroll when modal is open; made modal fixed instead of absolute as it did not look good when page was scrolled

--- src/modal.tsx
+++ src/modal.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDom from 'react-dom';
+import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
 
 const Portal = ({ children }: any) => {
   return ReactDom.createPortal(
@@ -9,10 +10,24 @@ const Portal = ({ children }: any) => {
 }
 
 export default function({ children } :any) {
+  const targetRef = React.createRef<HTMLInputElement>();
+
+  useEffect(() => {
+    if (targetRef.current) {
+      disableBodyScroll(targetRef.current)
+    }
+
+    return function cleanup() {
+      if (targetRef.current) {
+        enableBodyScroll(targetRef.current);
+      }
+    }
+  }, []);
+
   return (
     <Portal>
       <div className='backdrop'></div>
-      <div className='modal'>{children}</div>
+      <div ref={targetRef} className='modal'>{children}</div>
     </Portal>
   );
 }
